2165b3fa9d7efc8379220c65d2d2135fa60611bd
Jastrzab <kubixxu@gmail.com>
2022-01-11 19:30:58 +0100

    Changes in main_server

--- logic/main_server/app.py
+++ logic/main_server/app.py
@@ -1,13 +1,24 @@
 from flask import Flask
-from communication import receiver, sender
+from communication import receiver, sender, constants
 import threading
+
 
 broker = "localhost"
 
-receiver_api = receiver.Receiver(broker)
-sender_api = sender.Sender()
+receiver_api = receiver.Receiver(broker, constants.ROOM_DATA)
+sender_api = sender.Sender(broker, constants.DIRECTIVES)
+def process_message(client, userdata, message):
+    print(client + " send message " + message)
 
-def wait_for_connections():
+def run_receiver():
+    receiver_api.connect_to_broker()
+    receiver_api.client.on_message = process_message
+    receiver_api.loop_start()
+    receiver_api.subscribe()
+
+
+rec_thread = threading.Thread(target=run_receiver, )
+rec_thread.start()
 
 app = Flask(__name__)
 
