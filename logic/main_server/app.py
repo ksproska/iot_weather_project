03207270854f26e9637cc75333c3941ba4f4f8bc
Jastrzab <kubixxu@gmail.com>
2022-01-12 16:02:14 +0100

    Added database handling

--- logic/main_server/app.py
+++ logic/main_server/app.py
@@ -1,15 +1,37 @@
 import sys
+
 sys.path.append('/home/pi/Desktop/proj/iot_weather_project')
+from database.tables_as_classes import Record
 from flask import Flask
-from routes import routes, sender_api, receiver_api
+from routes import routes, sender_api, receiver_api, db_connection
 import threading
 
 
 sender_api.connect_to_broker()
 
 def process_message(client, userdata, message):
-    # 1. split message to get required information about room, temp and humidity
-    # 2. save records to database
+    # Split message to get required information about room, temp and humidity
+    message_decoded = str(message.payload.decode("utf-8")).split("#")
+    room_id = message_decoded[0]
+    temperature = float(message_decoded[1])
+    humidity = float(message_decoded[2])
+    pressure = int(message_decoded[3])
+
+    # Get informations from database about current preferences
+    current_pref_temp = db_connection.current_preference_temperature(room_id)
+    current_pref_hum = db_connection.current_preference_humidity(room_id)
+
+    # Save records to database
+    db_connection.add_object(Record.with_current_time(room_id, temperature, humidity, pressure, current_pref_temp > temperature, current_pref_hum < humidity))
+
+    is_thermostat_on = "true" if current_pref_temp > temperature else "false"
+    is_dryer_on = "true" if current_pref_hum < humidity else "false"
+
+    # If temperature is too low, send message that thermostat should be on
+    # If humidity is too high, send message that dryer should be on
+
+    sender_api.publish(f"{room_id}#{is_thermostat_on}#{is_dryer_on}")
+
     pass
 
 def run_receiver():
