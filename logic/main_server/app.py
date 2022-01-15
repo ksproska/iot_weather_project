3215abb01bda93562b3e3aca74cc31ba5ac36780
Kajetan Pynka <siema1234@gazeta.pl>
2022-01-15 22:38:22 +0100

    fixed paths

--- logic/main_server/app.py
+++ logic/main_server/app.py
@@ -1,51 +1,50 @@
 import sys
 
-sys.path.append('/home/kyatt/Desktop/uczelnia/IoT/iot_weather_project')
-# from database.tables_as_classes import Record
+sys.path.append('/home/pi/Desktop/proj/iot_weather_project')
+from database.tables_as_classes import Record
 from flask import Flask
-from routes import routes #,sender_api, receiver_api, db_connection
-# from database.connect import Connection
-# import threading
+from routes import routes, sender_api, receiver_api, db_connection
+from database.connect import Connection
+import threading
 
 
-# sender_api.connect_to_broker()
+sender_api.connect_to_broker()
 
-# def process_message(client, userdata, message):
-#     # Split message to get required information about room, temp and humidity
-#     connection = Connection()
-#     message_decoded = str(message.payload.decode("utf-8")).split("#")
-#     room_id = message_decoded[0]
-#     temperature = float(message_decoded[1])
-#     humidity = float(message_decoded[2])
-#     pressure = int(message_decoded[3])
+def process_message(client, userdata, message):
+    # Split message to get required information about room, temp and humidity
+    connection = Connection()
+    message_decoded = str(message.payload.decode("utf-8")).split("#")
+    room_id = message_decoded[0]
+    temperature = float(message_decoded[1])
+    humidity = float(message_decoded[2])
+    pressure = int(message_decoded[3])
 
-#     # Get informations from database about current preferences
-#     current_pref_temp = connection.current_preference_temperature(room_id)
-#     current_pref_hum = connection.current_preference_humidity(room_id)
+    # Get informations from database about current preferences
+    current_pref_temp = connection.current_preference_temperature(room_id)
+    current_pref_hum = connection.current_preference_humidity(room_id)
 
-#     # Save records to database
-#     connection.add_object(Record.with_current_time(room_id, temperature, humidity, pressure, current_pref_temp > temperature, current_pref_hum < humidity))
+    # Save records to database
+    connection.add_object(Record.with_current_time(room_id, temperature, humidity, pressure, current_pref_temp > temperature, current_pref_hum < humidity))
 
-#     is_thermostat_on = "true" if current_pref_temp > temperature else "false"
-#     is_dryer_on = "true" if current_pref_hum < humidity else "false"
+    is_thermostat_on = "true" if current_pref_temp > temperature else "false"
+    is_dryer_on = "true" if current_pref_hum < humidity else "false"
 
-#     # If temperature is too low, send message that thermostat should be on
-#     # If humidity is too high, send message that dryer should be on
+    # If temperature is too low, send message that thermostat should be on
+    # If humidity is too high, send message that dryer should be on
 
-#     sender_api.publish(f"{room_id}#{is_thermostat_on}#{is_dryer_on}")
-#     connection.close()
-#     pass
+    sender_api.publish(f"{room_id}#{is_thermostat_on}#{is_dryer_on}")
+    connection.close()
+    pass
 
-# def run_receiver():
-#     receiver_api.connect_to_broker()
-#     receiver_api.client.on_message = process_message
-#     receiver_api.loop_start()
-#     receiver_api.subscribe()
+def run_receiver():
+    receiver_api.connect_to_broker()
+    receiver_api.client.on_message = process_message
+    receiver_api.loop_start()
+    receiver_api.subscribe()
 
 
-# rec_thread = threading.Thread(target=run_receiver)
-# rec_thread.start()
+rec_thread = threading.Thread(target=run_receiver)
+rec_thread.start()
 
 app = Flask(__name__)
 app.register_blueprint(routes)
-
